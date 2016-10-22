b2091a62bea527e05f10a64feebb21201f7b8424
Konrad <gotfrydkonrad@gmail.com>
2016-10-22 22:46:02 +0200

    work save

--- vhdl/xgbe/counter.vhd
+++ vhdl/xgbe/counter.vhd
@@ -18,55 +18,54 @@ entity counter is
 end counter;
 
 architecture counter_arch of counter is
-	--cnt - packet count
-	signal cnt, cnt_tmp : unsigned(REG_WIDTH - 1 downto 0);
-	signal int_gen_cnt, int_gen_cnt_tmp  : unsigned(REG_WIDTH - 1 downto 0);
-	signal int_state  : std_logic := '0';
-begin
-    
-	cnt_out <= std_logic_vector(cnt);
+	signal cnt : unsigned(REG_WIDTH - 1 downto 0);
+	signal int_gen_cnt  : unsigned(REG_WIDTH - 1 downto 0);
+	signal incr_state : std_logic := '0';
 
+begin
+    	
 	process(clk) is
 	begin
 		if (rising_edge(clk)) then
 			if (resetn = '0') then
 				cnt <= (others => '0');
 				int_gen_cnt <= (others => '0');
+				interrupt <= '0';
+				cnt_out <= (others => '0');
 			else
-				cnt <= cnt_tmp;
-				int_gen_cnt <= int_gen_cnt_tmp;
-			end if;
-		end if;
-	end process;
+				cnt_out <= std_logic_vector(cnt);
+				cnt <= cnt;
 
-	process (cnt, incr, get_val) is
-	begin
-		cnt_tmp <= cnt;
-		if (get_val = '1') then
-			if (incr = '1') then
-				cnt_tmp <= to_unsigned(1, REG_WIDTH);
-			else
-				cnt_tmp <= (others => '0');
-			end if;
-		else
-			if (incr = '1') then
-				cnt_tmp <= cnt + 1;
+				if (get_val = '1') then
+					if (incr_state = '1' and incr = '1') then
+						cnt <= TO_UNSIGNED(2, REG_WIDTH);
+					elsif (incr_state = '1' or incr = '1') then
+						cnt <= TO_UNSIGNED(1, REG_WIDTH);
+					else
+						cnt <= (others => '0');
+					end if;
+					incr_state <= '0';
+				else
+					if (incr = '1') then
+						cnt <= cnt + 1;
+						incr_state <= '1';
+					else
+						incr_state <= '0';
+					end if;
+				end if;		
+				
+				interrupt <= '0';
+				int_gen_cnt <= int_gen_cnt;
+				if (incr = '1') then
+					interrupt <= '1';
+					int_gen_cnt <= (others => '0');
+				elsif (to_integer(int_gen_cnt) > INT_GEN_DELAY) then
+					interrupt <= '1';
+					int_gen_cnt <= (others => '0');
+				elsif (cnt /= 0) then
+					int_gen_cnt <= int_gen_cnt + 1;
+				end if;
 			end if;
 		end if;
-	end process;
-	
-	process (int_gen_cnt, cnt, incr) is
-	begin 
-	       interrupt <= '0';
-	       int_gen_cnt_tmp <= int_gen_cnt;
-	       if (incr = '1') then
-	           interrupt <= '1';
-	           int_gen_cnt_tmp <= (others => '0');
-	       elsif (to_integer(int_gen_cnt) > INT_GEN_DELAY) then
-	           interrupt <= '1';
-	           int_gen_cnt_tmp <= (others => '0');
-	       elsif (cnt /= 0) then
-		   int_gen_cnt_tmp <= int_gen_cnt + 1;
-	       end if;
 	end process;
 end counter_arch;
--- vhdl/xgbe/flag_over_clocks.vhd
+++ vhdl/xgbe/flag_over_clocks.vhd
@@ -23,8 +23,6 @@ signal sig_reg_out : std_logic_vector(2 downto 0) := (others => '0');
 
 begin
 
-flag_out <= sig_reg_out(2) xor sig_reg_out(1);
-
 process (clk_in) begin
 	if (rising_edge(clk_in)) then
 		if (clk_in_resetn = '0') then
@@ -39,8 +37,10 @@ process (clk_out) begin
 	if (rising_edge(clk_out)) then
 		if (clk_out_resetn = '0') then
 			sig_reg_out <= (others => '0');
+			flag_out <= '0';
 		else
 			sig_reg_out <= sig_reg_out(1 downto 0) & sig_reg_in;
+			flag_out <= sig_reg_out(2) xor sig_reg_out(1);
 		end if;
 	end if;
 end process;
